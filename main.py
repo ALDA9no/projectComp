ac697a7d349b90dac3b4040e03fe30ace3c28cef
User <sergioaotero26@hotmail.com>
2023-05-16 22:26:24 -0500

    falta arreglar nana

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
 
-def read_csv(scv):
-    pd.read_csv('table.csv')
+
+def plot_generes(data):
+    y = np.array(data['Genero'].values)
+    plt.hist(y)
+    plt.title("Generes")
+    plt.show()
+
+
+def plot_lack_of_food(data):
+    occ = Counter(data['Ha sufrido de escasez de alimentos'].values).most_common()[:20]
+    df = pd.DataFrame(occ, columns=['Model', 'Frequency'])
+    df.plot(kind='bar', x='Model')
+    plt.title("has food shortages")
+    plt.show()
+
+
+def plot_reason(data):
+    occ = Counter(data['Razon de la escasez de alimentos'].values).most_common()
+    df = pd.DataFrame(occ, columns=['Fuel type', 'Frequency'])
+    df.plot(kind='bar', x='Fuel type')
+    # df.drop('nan', inplace=True, axis=1)
+    plt.title("Reasons")
+    plt.show()
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('table.csv')
+    plot_generes(data)
+    plot_lack_of_food(data)
+    plot_reason(data)
